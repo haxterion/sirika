908efc433766a4dfcfe6a848d7bbe018e44ad990
Asterion Buckler <rijalimnida@gmail.com>
2019-06-08 02:18:16 +0700

    konfigurasi kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => App\Http\Middleware\Admin,
+        'operator' => App\Http\Middleware\Operator,
+        'supplier' => App\Http\Middleware\Supplier,
     ];
 
     /**
